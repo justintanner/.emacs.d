891ec9c14e835401bef77f26f2726625812e204a
justintanner <justinwtanner@gmail.com>
2017-11-28 12:14:11 -0600

    Half finished getting hammerspoon to work with ctrl+space

--- helpers/emacs_hammerspoon.lua
+++ helpers/emacs_hammerspoon.lua
@@ -1,13 +1,15 @@
 local keys = {
   ["globalOverride"] = {
     ["ctrl"] = {
-      ["t"] = {'cmd', 'tab', false, nil}, -- sort working
+      ["t"] = {'cmd', 'tab', false, nil}, -- sorta working
       ["j"] = {'cmd', 'space', false, nil}
     }
   },
   ["globalEmacs"] = {
     ["ctrl"] = {
-      ["s"] = {'cmd', 'f', false, nil}
+      ["s"] = {'cmd', 'f', false, nil},
+      ["n"] = {nil, 'down', true, nil},
+      ["space"] = {nil, nil, false, macroStartMark},
     }
   }
 }
@@ -16,11 +18,17 @@ local markActive = false
 local currentApp = nil
 local map = hs.hotkey.modal.new()
 
+map:bind('ctrl', 'space', nil, function() processKey('ctrl', 'space') end)
 map:bind('ctrl', 'j', nil, function() processKey('ctrl', 'j') end)
 map:bind('ctrl', 't', nil, function() processKey('ctrl', 't') end)
 map:bind('ctrl', 's', nil, function() processKey('ctrl', 's') end)
 
 map:enter()
+
+function macroStartMark()
+  hs.eventtap.keyStroke('shift')  
+  markActive = true
+end
 
 function processKey(mod, key)
   map:exit()
@@ -31,7 +39,7 @@ function processKey(mod, key)
   elseif (isEmacs()) then
     print("Letting " .. mod .. "+" .. key .. " passthrough to Emacs")
     hs.eventtap.keyStroke(mod, key)
-  elseif currentApp ~= nil and keys[currentApp][mod][key] ~= nil then
+  elseif currentApp ~= nil and keys[currentApp] ~= nil and keys[currentApp][mod][key] ~= nil then
     config = keys[currentApp][mod][key]
     changeKey(mod, key, config[1], config[2], config[3], config[4])
   elseif keys["globalEmacs"][mod][key] ~= nil then
@@ -42,14 +50,42 @@ function processKey(mod, key)
   map:enter()
 end
 
-function changeKey(mod, key, newMod, newKey, clearMark, macro)
+function changeKey(mod, key, newMod, newKey, holdShift, macro)
   if (macro ~= nil) then
-    print("Executing a macro " .. macro .. " for " .. mod .. "+" .. key)            
+    print("Executing a macro " .. macro .. " for " .. (mod or "") .. "+" .. (key or ""))           
     _G[macro]()
   else
-    print("Changing " .. mod .. "+" .. key .. " to " .. newMod .. "+" .. newKey)    
-    hs.eventtap.keyStroke(newMod, newKey)
+    print("Changing " .. mod .. "+" .. key .. " to " .. (newMod or "") .. "+" .. (newKey or ""))
+
+    if (holdShift) then
+      hs.eventtap.keyStroke(prepModifier(newMod, holdShift), newKey)
+    else
+      hs.eventtap.keyStroke(prepModifier(newMod, holdShift), newKey)
+    end
   end    
+end
+
+function prepModifier(mod, holdShift)
+  if holdShift then
+    return addShift(mod)
+  end
+
+  if type(mod) == 'string' then
+    return {mod}
+  end
+
+  return {}
+end
+
+function addShift(mod)
+  if type(mod) == 'string' then
+    return {'shift', mod}
+  elseif type(mod) == 'table' then
+    table.insert(mod, 1, 'shift')
+    return mod
+  end
+  
+  return {'shift'}
 end
 
 function isEmacs()
