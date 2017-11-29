7af0782cb9d80ff1dad2279ee138d4d633232524
justintanner <justinwtanner@gmail.com>
2017-11-29 11:41:50 -0600

    Started work on hammerspoon x prefix

--- helpers/emacs_hammerspoon.lua
+++ helpers/emacs_hammerspoon.lua
@@ -1,67 +1,80 @@
 local keys = {
-  ["globalOverride"] = {
-    ["ctrl"] = {
-      ["t"] = {'cmd', 'tab', false, nil}, -- sorta working
-      ["j"] = {'cmd', 'space', false, nil}
+  ['globalOverride'] = {
+    ['ctrl'] = {
+      ['t'] = {'cmd', 'tab', false, nil}, -- sorta working
+      ['j'] = {'cmd', 'space', false, nil}
     }
   },
-  ["globalEmacs"] = {
-    ["ctrl"] = {
-      ["s"] = {'cmd', 'f', false, nil},
-      ["n"] = {nil, 'down', true, nil},
-      ["space"] = {nil, nil, false, macroStartMark},
+  ['globalEmacs'] = {
+    ['ctrl'] = {
+      ['s'] = {'cmd', 'f', false, nil},
+      ['n'] = {nil, 'down', true, nil},
+      ['x'] = {nil, nil, false, 'macroStartCtrlX'},     
+      ['space'] = {nil, nil, false, 'macroStartCtrlSpace'},
+    }
+  }
+  ['xPrefix'] = {
+    ['ctrl'] = {
+      ['b'] = { } -- Finish me!
     }
   }
 }
 
-local markActive = false
+local ctrlXActive = false
+local ctrlSpaceActive = false
 local currentApp = nil
 local map = hs.hotkey.modal.new()
 
 map:bind('ctrl', 'space', nil, function() processKey('ctrl', 'space') end)
 map:bind('ctrl', 'j', nil, function() processKey('ctrl', 'j') end)
+map:bind('ctrl', 'n', nil, function() processKey('ctrl', 'n') end)
 map:bind('ctrl', 't', nil, function() processKey('ctrl', 't') end)
 map:bind('ctrl', 's', nil, function() processKey('ctrl', 's') end)
+map:bind('ctrl', 'x', nil, function() processKey('ctrl', 'x') end)
 
 map:enter()
 
-function macroStartMark()
-  hs.eventtap.keyStroke('shift')  
-  markActive = true
+function macroStartMarkSpace()
+  hs.eventtap.keyStroke({}, 'shift')  
+  ctrlSpaceActive = true
+end
+
+function marcroStartCtrlX()
+  ctrlXActive = true
+
+  hs.timer.doAfter(0.75, function() ctrlXActive = false end)
 end
 
 function processKey(mod, key)
   map:exit()
 
-  if keys["globalOverride"][mod][key] ~= nil then
-    config = keys["globalOverride"][mod][key]
+  if keys['globalOverride'][mod][key] ~= nil then
+    config = keys['globalOverride'][mod][key]
     changeKey(mod, key, config[1], config[2], config[3], config[4])    
   elseif (isEmacs()) then
-    print("Letting " .. mod .. "+" .. key .. " passthrough to Emacs")
+    print('Letting ' .. mod .. '+' .. key .. ' passthrough to Emacs')
     hs.eventtap.keyStroke(mod, key)
   elseif currentApp ~= nil and keys[currentApp] ~= nil and keys[currentApp][mod][key] ~= nil then
     config = keys[currentApp][mod][key]
     changeKey(mod, key, config[1], config[2], config[3], config[4])
-  elseif keys["globalEmacs"][mod][key] ~= nil then
-    config = keys["globalEmacs"][mod][key]
+  elseif keys['globalEmacs'][mod][key] ~= nil then
+    config = keys['globalEmacs'][mod][key]
     changeKey(mod, key, config[1], config[2], config[3], config[4])
   end
   
   map:enter()
 end
 
-function changeKey(mod, key, newMod, newKey, holdShift, macro)
-  if (macro ~= nil) then
-    print("Executing a macro " .. macro .. " for " .. (mod or "") .. "+" .. (key or ""))           
+function changeKey(mod, key, newMod, newKey, ctrlSpaceSensitive, macro)
+  if macro ~= nil then
     _G[macro]()
+    print('Executing a macro ' .. macro .. ' for ' .. (mod or '') .. '+' .. (key or ''))              
   else
-    print("Changing " .. mod .. "+" .. key .. " to " .. (newMod or "") .. "+" .. (newKey or ""))
-
-    if (holdShift) then
-      hs.eventtap.keyStroke(prepModifier(newMod, holdShift), newKey)
-    else
-      hs.eventtap.keyStroke(prepModifier(newMod, holdShift), newKey)
-    end
+    holdShift = (ctrlSpaceSensitive and ctrlSpaceActive)
+    if holdShift then print 'Holding shift' end
+    hs.eventtap.keyStroke(prepModifier(newMod, holdShift), newKey)
+    
+    print('Changing ' .. mod .. '+' .. key .. ' to ' .. (newMod or '') .. '+' .. (newKey or ''))
   end    
 end
 
@@ -115,6 +128,4 @@ function dump(o)
   end
 end
 
--- hs.timer.doAfter(0.5, function()
---   print 'timer is up!'                   
--- end)
+
